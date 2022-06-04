e06150255056849d106565d0532c91057c490a6d
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-04 18:57:46 +0300

    create template files to handle the registration, login and home page

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('', include('shavogram.urls'))
+
 ]
--- shavogram/views.py
+++ shavogram/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Photo
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url = "accounts/login")
+def index(request):
+    images = Photo.objects.all()
+    return render(request,"index.html", {"images":images})
+
