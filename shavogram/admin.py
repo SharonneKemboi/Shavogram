86c3cb43c626a742c01c36a435f6d21675acc981
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-05 21:47:25 +0300

    Register models

--- shavogram/admin.py
+++ shavogram/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, Comments,Follow,Profile
 
 # Register your models here.
 admin.site.register(Photo)
+admin.site.register(Comments)
+admin.site.register(Follow)
+admin.site.register(Profile)
--- shavogram/apps.py
+++ shavogram/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class ShavogramConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'shavogram'
