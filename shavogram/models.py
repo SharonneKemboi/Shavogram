80b80882fe8b50b9f5fe412251791ab8ccc286a5
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-05 21:42:25 +0300

    Add helper methods in model

--- shavogram/models.py
+++ shavogram/models.py
@@ -1,23 +1,68 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Photo(models.Model):
-    image_url = models.ImageField(upload_to = 'pictures')
-    name = models.CharField(unique = True,max_length = 31)
-    caption = models.CharField(max_length = 50)
-    likes = models.ManyToManyField(User, related_name = "likes")
-    user = models.ForeignKey(User,on_delete=models.CASCADE,)
+    image_url = models.ImageField(upload_to = 'pictures' , blank = True)
+    name = models.CharField(max_length = 35, blank = True)
+    caption = models.CharField(max_length = 55, blank = True)
+    likes = models.ManyToManyField(User, related_name = "likes", blank = True)
+    user = models.ForeignKey(User, related_name = "posts", on_delete=models.CASCADE, blank=True)
+    pub_date = models.DateTimeField(auto_now_add = True, blank = True)
+
+    def save_photo(self):
+        self.save()
+
+  
+    def delete_photo(self):
+      Photo.objects.get(id = self.id).delete()
+
+    def update_caption(self,new_caption):
+        self.caption = new_caption
+        self.save()
 
 class Profile(models.Model):
     user = models.OneToOneField(User,related_name = "profile",on_delete=models.CASCADE,)
-    bio = models.CharField(max_length = 40)
-    picture = models.ImageField(upload_to = 'pictures')
+    bio = models.CharField(default="Welcome", max_length = 40)
+    picture = models.ImageField(upload_to = 'pictures', blank = True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    
+    @classmethod
+    def search_user(cls,name):
+        return User.objects.filter(username__icontains = name) 
 
 class Comments(models.Model):
+    mycomments = models.CharField(max_length = 100, blank = True)
     photo = models.ForeignKey(Photo, related_name = "comments",on_delete=models.CASCADE,)
     user = models.ForeignKey(User, related_name = "comments",on_delete=models.CASCADE,)
+    
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        Comments.objects.get(id = self.id).delete()
+    
+    def update_comment(self,new_comment):
+        mycomments = Comments.objects.get(id = self.id)
+        mycomments.comment = new_comment
+        mycomments.save()
 
 class Follow(models.Model):
-    user = models.ForeignKey(User, related_name = "followers", on_delete=models.CASCADE,)
-    followed_by = models.ForeignKey(User, related_name = "following", on_delete=models.CASCADE,)     
+    user = models.ForeignKey(User, related_name = "user_followers", on_delete=models.CASCADE,)
+    followed_by = models.ForeignKey(User, related_name = "user_following", on_delete=models.CASCADE,)     
+
+class identifiers(models.Model):
+    identifier = models.CharField(max_length = 30, null = True)    
+
